49c95eead11a4faa8cf8976fcd39882df21afeb9
shiv-u <shivshankar.hc@gmail.com>
2018-07-13 13:52:18 +0530

    updated main activity.
    Have removed extra whitesapces .

--- app/src/main/java/com/androidexample/courtcounter/MainActivity.java
+++ app/src/main/java/com/androidexample/courtcounter/MainActivity.java
@@ -1,20 +1,15 @@
 package com.androidexample.courtcounter;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-    int scorea = 0, scoreb = 0;
-
-    /**
-     * Displays the given score for Team A.
-     */
+    int scorea = 0;
+    int scoreb = 0;
 
     RelativeLayout parent;
 
@@ -28,50 +23,41 @@ public class MainActivity extends AppCompatActivity {
     public void scoreSixa(View view) {
         scorea = scorea + 6;
         displaya(scorea);
-
-
     }
 
     public void scoreFoura(View view) {
         scorea = scorea + 4;
         displaya(scorea);
-
     }
 
     public void scoreOnea(View view) {
         scorea = scorea + 1;
         displaya(scorea);
-
     }
 
     public void displaya(int number) {
-        TextView countViewera = (TextView) findViewById(R.id.team_a_score);
+        TextView countViewera =  findViewById(R.id.team_a_score);
         parent.setBackgroundColor(getResources().getColor(R.color.RCB));
         countViewera.setText("" + number);
     }
 
     public void scoreSixb(View view) {
         scoreb += 6;
-
         displayb(scoreb);
-
-
     }
 
     public void scoreFourb(View view) {
         scoreb += 4;
         displayb(scoreb);
-
-    }
+            }
 
     public void scoreOneb(View view) {
         scoreb += 1;
         displayb(scoreb);
-
     }
 
     public void displayb(int number) {
-        TextView countViewerb = (TextView) findViewById(R.id.team_b_score);
+        TextView countViewerb =  findViewById(R.id.team_b_score);
         parent.setBackgroundColor(getResources().getColor(R.color.CSK));
         countViewerb.setText("" + number);
     }
